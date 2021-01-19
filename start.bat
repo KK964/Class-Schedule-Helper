2bbfb06683567390ff13a660e70de35040a14518
KK964 <morrkade05@gmail.com>
2021-01-19 09:31:20 -0600

    added start script

---
+++ start.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Installing/updating bot dependencies
+call npm ci --only=production --loglevel=warn >NUL
+
+if NOT ["%errorlevel%"]==["0"] (
+  pause
+  exit /b %errorlevel%
+)
+
+echo Starting the tool
+call npm run start
+
+if NOT ["%errorlevel%"]==["0"] (
+  pause
+  exit /b %errorlevel%
+)
